759eb72fa42250317b23770a41ad0dad85b93e0b
Sourabh Mishra <sourabh19278@iiitd.ac.in>
2025-02-10 22:43:35 +0530

    added upload api in frontedn

--- query-genie-backend/app.py
+++ query-genie-backend/app.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from service.queryGenerator import generateQuery
-from service.queryExecutor import executeCursor,getCreateTableQuery
+from service.queryExecutor import executeCursor, getCreateTableQuery
 from dotenv import load_dotenv
 
-
-
-
 app = FastAPI()
+
+# Enable CORS (Allow requests from frontend or other origins)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins (change this in production)
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 class QueryPayload(BaseModel):
     query: str
@@ -30,12 +37,12 @@ async def process_query(payload: QueryPayload):
     create_table_query = getCreateTableQuery('Test.csv')
     generated_query = generateQuery(create_table_query, query_text)
     print(f"Generated query: {generated_query}")
-    if ( generated_query.startswith("Error") ):
+    
+    if generated_query.startswith("Error"):
         return {"error": generated_query}
     else:
         output = executeCursor(generated_query)
         return {"output": output}
-    
 
 if __name__ == "__main__":
     import uvicorn
